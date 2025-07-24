89c2b2bff99c54de83f75290e52a6130cc2e50fd
mrodavis <orville.davis@scrapit.io>
2025-07-24 13:29:47 -0400

    complete new application functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -18,5 +18,49 @@ router.get('/', async (req, res) => {
   }
 });
 
+// controllers/applications.js
+
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+// controllers/applications.js`
+
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// controllers/applications.js
+
+router.get('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Render index.ejs, passing in all of the current user's
+    // applications as data in the context object.
+    res.render('applications/index.ejs', {
+      applications: currentUser.applications,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -9,8 +30,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
+
+
 const User = mongoose.model("User", userSchema);
+
+
+
+
 
 module.exports = User;
